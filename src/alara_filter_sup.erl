393c437fb0d16242554532b7f7a0e752515ef917
Steve Roques <steve.roques@gmail.com>
2026-05-28 12:42:08 +0200

    feat: migrate to Phase 3 (em_pop gossip + Cowboy HTTP)
    Replace em_filter:start_agent/3 WebSocket pattern with:
    - OTP supervisor (alara_filter_sup)
    - gen_server handler (alara_filter_server) with persistent Memory state
    - em_pop_sup:start_node/2 for gossip peer discovery
    - Cowboy HTTP via em_filter_http for direct queries
    
    Update em_filter dep: 1.2.4 -> 1.2.5
    Add config/sys.config with ports and gossip seeds

---
+++ src/alara_filter_sup.erl
@@ -0,0 +1,23 @@
+%%%-------------------------------------------------------------------
+%%% @doc alara_filter supervisor.
+%%%
+%%% Supervises the alara_filter_server gen_server.
+%%% @end
+%%%-------------------------------------------------------------------
+-module(alara_filter_sup).
+-behaviour(supervisor).
+
+-export([start_link/0, init/1]).
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init([]) ->
+    ServerSpec = #{
+        id      => alara_filter_server,
+        start   => {alara_filter_server, start_link, []},
+        restart => permanent,
+        type    => worker
+    },
+    {ok, {#{strategy => one_for_one, intensity => 3, period => 10},
+          [ServerSpec]}}.
